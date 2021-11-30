d1f8cf0cbbf352cbe82c21cae150a84e9f87e636
petit <fayembad@gmail.com>
2021-11-30 21:29:32 +0100

    initialProject

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-console.log("hello petit ")
+import express from "express" ;
+import dotenv from "dotenv" ; 
+import morgan from "morgan" ;
+import mongoose from "mongoose" ;
+import cors from "cors"
+
+const app = express() ;  
+
+dotenv.config() ; 
+
+const port = process.env.PORT ; 
+const api = process.env.API_URl  ;  
+
+// Middleware  
+app.use(cors()) 
+
+app.use(express.urlencoded({
+    extended : true
+})) 
+app.use(express.json()) 
+
+// morgan pour enrigistrer les requetes http pour node js (utilisation d'une chaine de format prédéfinie)
+app.use(morgan('tiny')) 
+
+//connection avec ma base de donnée .
+mongoose.connect(process.env.CONNECTION_STRING,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}) 
+.then(()=> console.log("connection à la base de donnée réussie"))
+.catch(()=>console.log("connection à la base donnée échouée "))
+app.get(`${api}/products` ,(req,res)=>{ 
+    
+    res.send("hello")
+}) 
+
+
+app.listen(port ,()=>{ 
+    console.log(`Le serveur est lancé sur le port ${port}`)
+})
