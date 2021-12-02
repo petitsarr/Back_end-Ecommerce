b12c186c21af8bfe11c8c8e951608a9f64cd08e3
petit <fayembad@gmail.com>
2021-12-02 05:42:51 +0100

    Backend_product_and_category

--- models/category.js
+++ models/category.js
@@ -13,5 +13,13 @@ const categorySchema = new mongoose.Schema({
         type : String 
 
     }
-}) 
+})  
+// Pour transformer _id en id uniquement 
+categorySchema.method('toJSON', function(){
+    const { __v, ...object } = this.toObject();
+    const { _id:id, ...result } = object;
+    return { ...result, id };
+});
+
+
 export default categorySchema ;
--- routes/products.js
+++ routes/products.js
@@ -1,11 +1,16 @@
 import express from "express" 
-import { addNewProduct , updateProductById ,getAllProduct , getProductById} from "../controllers/product" 
+import { addNewProduct , getProductsFeatures ,getProductCount , updateProductById ,getAllProduct , deleteProductById , getProductById} from "../controllers/product" 
 
  const routerProduct = express.Router() ;
 
- routerProduct.post("/products" , addNewProduct) ; 
- routerProduct.get("/products" ,getAllProduct) 
+ routerProduct.post("/products" , addNewProduct) ;  
+ //http://localhost:5000/api/v1/products?categories=61a6ecdf3a0af134ddce465b
+ routerProduct.get("/products" ,getAllProduct)  ; 
+ routerProduct.get("/products/count" , getProductCount) 
+ routerProduct.get("/products/features/:count" , getProductsFeatures)   
  routerProduct.get("/products/:idpro" , getProductById) 
  routerProduct.put("/products/:idpro",updateProductById)
+ routerProduct.delete("/products/:idpro", deleteProductById )
+
 
  export default routerProduct ;
